7532f3cf7ab67b133f50602288a0b20b63e912ac
GiferElem <3895736191@qq.com>
2025-09-27 03:07:35 +0800

    First commit : Realize player control

---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,38 @@
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    private PlayInput playInput;
+    private Vector2 inputDirection;
+    private Rigidbody rb;
+
+    private float moveSpeed = 5f;
+    private float rotateSpeed = 10f;
+
+    private void Awake() {
+        playInput = new PlayInput();
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnEnable() {
+        playInput.Enable();
+    }
+    private void OnDisable() {
+        playInput.Disable();
+    }
+    private void FixedUpdate() {
+        inputDirection = playInput.GamePlay.Move.ReadValue<Vector2>().normalized;
+        Move(inputDirection);
+    }
+    private void Move(Vector2 dir) {
+        if (dir.magnitude > 0.1f) {
+            Vector3 moveDir = new Vector3(dir.x, 0, dir.y);
+            rb.velocity = new Vector3(moveDir.x * moveSpeed, rb.velocity.y, moveDir.z * moveSpeed);
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.fixedDeltaTime);
+        } else {
+            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        }
+    }
+
+}
